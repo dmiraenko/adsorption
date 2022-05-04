4932a4f60638c06dbe6d419ebeddb071e83cdd33
Dmitrii <dmiraenko@gmail.com>
2022-05-04 18:53:01 +0300

    [F] Fast IAS instability

--- IAS.py
+++ IAS.py
@@ -18,37 +18,34 @@ class IAS():
 
         return [(total_pressure * y[i] / self.funcs[i].pure_comp_pressure(z)).to('').magnitude for i in range(len(y))]
 
-    def fast_IAS(self, total_pressure, y):
-        ct = total_pressure * sum([self.funcs[i].henry_law_const * y[i] for i in range(len(y))])
-        p0 = [0.01 * ct / f.henry_law_const for f in self.funcs]
-        print(p0)
+    def fast_IAS(self, total_pressure, y, max_iter = 100, eps = 1e-7):
+        s = sum([self.funcs[i].henry_law_const * y[i] for i in range(len(y))])
+        x = [self.funcs[i].henry_law_const * y[i] / s for i in range(len(y))]
 
         it = 0
-        eps = 1e-7
-        max_iter = 1000
         while(True):
+            p0 = [total_pressure * y[i] / x[i] for i in range(len(y))]
             z0 = self.funcs[0].red_spr_pressure(p0[0])
-            g = [sum([y[i] / p0[i] for i in range(len(y))]) - 1 / total_pressure] + \
+            g = [sum(x) - 1] + \
                 [z0 - self.funcs[i].red_spr_pressure(p0[i]) for i in range(1, len(y))]
 
-            j = [self.funcs[0].surface_conc(p0[0]) / p0[0]] + \
-                [y[i] / (self.funcs[i].surface_conc(p0[i]) * p0[i]) for i in range(1, len(y))]
+            j = [self.funcs[0].surface_conc(p0[0]) / x[0]] + \
+                [x[i] / self.funcs[i].surface_conc(p0[i]) for i in range(1, len(y))]
 
-            fsum = 0
+            s = 0
             for i in range(1, len(y)):
-                fsum += j[i]
+                s += j[i]
                 g[0] -= j[i] * g[i]
             
-            g[0] /= - y[0] / p0[0]**2 - j[0] * fsum
-            g[1:] = [(j[0] * g[0] - g[i]) * j[i] * p0[i]**2 / y[i] for i in range(1, len(y))]
+            g[0] /= 1 + j[0] * s
+            g[1:] = [(g[i] + g[0] * j[0]) * j[i] for i in range(1, len(y))]
 
-            p0 = [k if (k := p0[i] - g[i]).magnitude > 0 else eps * total_pressure.units for i in range(len(y))]
-            print(p0)
+            x = [k if (k := x[i] - g[i]).magnitude > 0 else eps for i in range(len(y))]
 
-            e = sqrt(sum([((g[i] / p0[i]).to("").magnitude)**2 for i in range(len(y))]))
+            e = sqrt(sum([((g[i] / x[i]).to("").magnitude)**2 for i in range(len(y))]))
             it += 1
             if (e < eps):
-                return p0
+                return x
             if (it > max_iter):
                 raise TimeoutError("Number of iterations exceeded the maximum.")
             
@@ -56,8 +53,8 @@ class IAS():
 if __name__ == "__main__":
 
     ureg = UnitRegistry()
-    total_pressure = 700 * ureg.kPa
-    y = [0.25, 0.25, 0.25, 0.25] * ureg.dimensionless
+    total_pressure = 7 * ureg.Pa
+    y = [0.37, 0.13, 0.01, 0.49] * ureg.dimensionless
     cmus = [32.47, 82.34, 584.43, 30.15] * ureg.mmol / ureg.gram
     b = [0.255, 2.7682, 97.7962, 23.703] * (1 / ureg.MPa)
     t = [0.777, 0.323, 0.134, 0.660] * ureg.dimensionless
@@ -66,7 +63,7 @@ if __name__ == "__main__":
 
     ias = IAS(dat)
 
-    print(ias.fast_IAS(total_pressure, y))
+    print([p.to('').magnitude for p in ias.fast_IAS(total_pressure, y)])
 
 
 
